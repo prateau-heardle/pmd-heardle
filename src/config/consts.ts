a564765ace12887509f654cd039617f771425845
prateau <philippe.rateau@outlook.com>
2026-01-25 18:49:33 +0100

    feat: :tada: init pmd-heardle

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -7,9 +7,9 @@ export const HEARDLE_SPLITS = [1, 2, 4, 7, 11, 16] // Each number on the list is
 export const START_DATE = "2024-02-03T00:00" // Arbitrary original start date, used to know the current heardle id
 
 export enum LocalStorageKeys {
-    SHOW_HELP = 'reactHeardle__showHelp',
-    GAME_STATE = 'reactHeardle__gameState',
-    GAME_STATE_INFINITE = 'reactHeardle__gameStateInfinite'
+    SHOW_HELP = 'pmdHeardle__showHelp',
+    GAME_STATE = 'pmdHeardle__gameState',
+    GAME_STATE_INFINITE = 'pmdHeardle__gameStateInfinite'
 }
 
 const sortMusicById = (music1: MusicElement, music2: MusicElement): number => music1.id - music2.id
